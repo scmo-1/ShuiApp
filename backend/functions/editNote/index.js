5daaff004196b9979fca5953c36f26c0d6c9a071
Simon Olsson <scmolsson@pm.me>
2025-09-30 10:26:56 +0200

    update serverless.yml with getNoteByNoteId

--- backend/functions/editNote/index.js
+++ backend/functions/editNote/index.js
@@ -1,3 +1,7 @@
+import { dbClient } from "../../services/db.js";
+import { response } from "../../utils/response.js";
+import { UpdateItemCommand } from "@aws-sdk/client-dynamodb";
+
 export const handler = async (event) => {
   return {
     statusCode: 200,
