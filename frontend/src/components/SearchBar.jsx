66ed90b54f6ff5e26302a73779c85a345241079e
Simon Olsson <scmolsson@pm.me>
2025-10-03 09:46:59 +0200

    add animated search bar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 import { Search } from "lucide-react";
 import { useState } from "react";
+import { motion } from "motion/react";
 
 function SearchBar({ handleSearch }) {
   const [query, setQuery] = useState("");
+  const [focus, setFocus] = useState(false);
 
   return (
-    <form onSubmit={(e) => handleSearch(e, query)}>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button>
-        <Search />
-      </button>
+    <form onSubmit={(e) => handleSearch(e, query)} className="">
+      <div className="relative flex items-center gap-3  outline-2 outline-black rounded-full">
+        <motion.label
+          for="name"
+          className="absolute z-2 left-4 bg-[#846b56] text-black"
+          initial={{ y: 0 }}
+          animate={focus ? { y: -24 } : { y: 0 }}
+        >
+          namn
+        </motion.label>
+        <input
+          id="name"
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
+          className="focus:outline-none  px-4 py-2 rounded-full"
+        />
+        <button className="bg-black rounded-full p-3 cursor-pointer">
+          <Search size={20} />
+        </button>
+      </div>
     </form>
   );
 }
